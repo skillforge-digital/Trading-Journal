663953112770d920dfce038bd7e440a0e9a944c7
skillforge-digital <skillforgeco.digital@gmail.com>
2026-01-29 15:21:00 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'skillforge-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/lucide@latest',
+  'https://unpkg.com/lightweight-charts/dist/lightweight-charts.standalone.production.js',
+  'https://www.gstatic.com/firebasejs/11.1.0/firebase-app-compat.js',
+  'https://www.gstatic.com/firebasejs/11.1.0/firebase-auth-compat.js',
+  'https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore-compat.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
